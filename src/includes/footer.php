cb8b7a5c9e6ac66df9a5fce6fa0bcc4fb7fc8f3a
Jelle S <jelle.s3112@gmail.com>
2022-10-30 18:46:54 +0100

    fix

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -12,20 +12,6 @@ function openInfo(evt, infoName) {
   document.getElementById(infoName).style.display = "block";
   evt.currentTarget.className += " active";
 }
-
-function openInfo2(evt, infoName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent2");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks2");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(infoName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
 </script>
 
 </body>
